bda00c47d028441cbb546370a81de5b853fe6be8
galeinav-lab <galeinav1999@gmail.com>
2026-01-09 17:36:33 +0200

    more info details and ui for more info and updates to manager

--- dist/coin-info.js
+++ dist/coin-info.js
@@ -1,1 +1,8 @@
-"use strict";
+export class CoinInfo {
+    constructor(imageUrl, usd, eur, ils) {
+        this.imageUrl = imageUrl;
+        this.usd = usd;
+        this.eur = eur;
+        this.ils = ils;
+    }
+}
---
+++ src/coin-info.ts
@@ -0,0 +1,3 @@
+export class CoinInfo {
+    constructor(public imageUrl: string, public usd: number, public eur: number, public ils: number) {}
+}
